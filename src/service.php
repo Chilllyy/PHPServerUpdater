9d8280812d8058a8bdd3a749c86b5b78b68b7f91
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2026-06-27 14:22:50 -0600

    test changes

--- src/service.php
+++ src/service.php
@@ -1,9 +1,12 @@
 <?php
 include __DIR__ . '/../vendor/autoload.php';
 
+set_time_limit(0);
+
 while (true) {
     print("Checking Queue Worker!");
     $path = realpath(__DIR__ . '/worker.php');
-    shell_exec("php $path");
+    require __DIR__ . '/worker.php';
+    #shell_exec("php $path");
     sleep(5);
 }
