560a444582da552ca273c4bec058fb17b0242291
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2026-06-27 12:25:49 -0600

    chang br to newine

--- src/worker.php
+++ src/worker.php
@@ -7,16 +7,16 @@ include __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/Util/Init.php';
 
 $file = '/tmp/queue.yml';
-echo "Loading Queue File";
+echo "Loading Queue File\n";
 $yaml = Yaml::parseFile($file);
 if (!array_key_exists('id', $yaml)) {
     die("No Queue to run");
 }
-echo "Loaded Queue!<br>";
+echo "Loaded Queue!\n";
 $upload_id = $yaml['id'];
 echo "Found Upload ID: " , $upload_id;
-echo "<br>Creating Pterodactyl Worker";
+echo "\nCreating Pterodactyl Worker";
 $pterodactyl = new Pterodactyl();
-echo "Created Worker<br>";
+echo "Created Worker\n";
 $pterodactyl->stopServer();
-echo "Stopping Server<br>";
+echo "Stopping Server\n";
