cb1e9ea4c0969899d685cdda8f966dc877cdb94d
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2026-06-27 12:24:35 -0600

    add logging to worker

--- src/worker.php
+++ src/worker.php
@@ -7,11 +7,16 @@ include __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/Util/Init.php';
 
 $file = '/tmp/queue.yml';
+echo "Loading Queue File";
 $yaml = Yaml::parseFile($file);
 if (!array_key_exists('id', $yaml)) {
     die("No Queue to run");
 }
+echo "Loaded Queue!";
 $upload_id = $yaml['id'];
-
+echo "Found Upload ID: " , $upload_id;
+echo "Creating Pterodactyl Worker";
 $pterodactyl = new Pterodactyl();
+echo "Created Worker";
 $pterodactyl->stopServer();
+echo "Stopping Server";
