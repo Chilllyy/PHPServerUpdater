5dec58dbadabd3e6f8a305c06480478622b407c6
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2026-06-27 14:34:25 -0600

    add newlines

--- src/service.php
+++ src/service.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/Util/Init.php';
 set_time_limit(0);
 
 while (true) {
-    print("Checking Queue Worker!");
+    print("Checking Queue Worker!\n\n");
     $path = realpath(__DIR__ . '/worker.php');
     require __DIR__ . '/worker.php';
     shell_exec("php $path");
