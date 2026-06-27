889609618ac69ca868269f050664ab7279ccd71f
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2026-06-27 14:27:12 -0600

    change back to shell exec

--- src/service.php
+++ src/service.php
@@ -6,7 +6,6 @@ set_time_limit(0);
 while (true) {
     print("Checking Queue Worker!");
     $path = realpath(__DIR__ . '/worker.php');
-    require __DIR__ . '/worker.php';
-    #shell_exec("php $path");
+    shell_exec("php $path");
     sleep(5);
 }
