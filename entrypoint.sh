ea429506ee72d153af68b9237daf6efd2e6f2ec1
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2026-06-27 18:53:59 -0600

    test entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,11 @@ echo "Starting Apache Webserver..."
 
 PORT="${APACHE_PORT:-80}"
 
-sed -i "s/^Listen 80/c\Listen $PORT" /usr/local/apache2/conf/httpd.conf
-
-apache2-foreground &
-
-echo "Waiting For Apache..."
-sleep 2
-
 echo "Launching Queue Runner..."
 php /home/container/src/service.php
+
+sed -i "s/Listen 80/Listen {{SERVER_PORT}}/g" /etc/apache2/ports.conf 
+sed -i "s/<VirtualHost \*:80>/<VirtualHost \*:{{SERVER_PORT}}>/g" /etc/apache2/sites-available/000-default.conf 
+export APACHE_RUN_USER=container 
+export APACHE_RUN_GROUP=container 
+apache2-foreground && ${STARTUP}
